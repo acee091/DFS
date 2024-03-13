3f28ab1b6e3273f43b8205aa7f5e3507a0dbc3c1
acee091 <aceevangelista091@gmail.com>
2024-03-13 16:57:23 -0300

    t

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine','handlebars')
+
+app.get('/', function(req, res){
+    const shop = {
+        name: 'São paulo',
+        year: '1930'
+    }
+    res.render('home', {shop: shop})
+})
+app.get('/login', function(req, res){
+    const products = ['Blusa', 'Camiseta', 'Calça']
+    res.render('login', {products: products, auth: false})
+})
+app.use(express.static('public'))
+app.listen(5000)
+
