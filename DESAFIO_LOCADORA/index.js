c35fa6f926c852dc40659e21be2cd7b625a06dd5
acee091 <aceevangelista091@gmail.com>
2024-03-16 18:59:24 -0300

    i

---
+++ DESAFIO_LOCADORA/index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res){
+    res.render('home')
+})
+// app.post('/cadastro', function(req, res){
+//     const nome = req.body.nome
+//     const endereco = req.body.endereco
+//     const telefone = req.body.telefone
+//     const email = req.body.email
+//     const auth = true
+//     const query = `INSERT INTO tbl_cliente ?? = ?, ?? = ?
+//     ?? = ?, ?? = ?`
+//     const data = ['nome', nome, 'endereco', endereco, 
+//     'telefone', telefone, 'email', email]
+
+//     pool.query(query, data, function (err, data) {
+//         if (err) {
+//           console.log(err)
+//         }
+    
+//         const book = data[0]
+    
+//         console.log(data[0])
+//         res.redirect(`/`, auth)
+//     })
+// })
+
+app.get('/cadastro', function(req, res){
+    res.render('cadastro')
+})
+app.listen(5000, function() {
+    console.log("Server is running on port " + 5000);
+});
