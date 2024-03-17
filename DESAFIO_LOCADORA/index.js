01227648191ea19497f16cd60638d8d147081fea
acee091 <aceevangelista091@gmail.com>
2024-03-16 22:53:05 -0300

    t

--- DESAFIO_LOCADORA/index.js
+++ DESAFIO_LOCADORA/index.js
@@ -11,28 +11,28 @@ app.use(express.static('public'))
 app.get('/', function(req, res){
     res.render('home')
 })
-// app.post('/cadastro', function(req, res){
-//     const nome = req.body.nome
-//     const endereco = req.body.endereco
-//     const telefone = req.body.telefone
-//     const email = req.body.email
-//     const auth = true
-//     const query = `INSERT INTO tbl_cliente ?? = ?, ?? = ?
-//     ?? = ?, ?? = ?`
-//     const data = ['nome', nome, 'endereco', endereco, 
-//     'telefone', telefone, 'email', email]
+app.post('/cadastro', function(req, res){
+    const nome = req.body.nome
+    const endereco = req.body.endereco
+    const telefone = req.body.telefone
+    const email = req.body.email
 
-//     pool.query(query, data, function (err, data) {
-//         if (err) {
-//           console.log(err)
-//         }
+    const query = `INSERT INTO tbl_cliente ?? = ?, ?? = ?
+    ?? = ?, ?? = ?`
+    const data = ['nome', nome, 'endereco', endereco, 
+    'telefone', telefone, 'email', email]
+
+    pool.query(query, data, function (err, data) {
+        if (err) {
+          console.log(err)
+        }
     
-//         const book = data[0]
+        const book = data[0]
     
-//         console.log(data[0])
-//         res.redirect(`/`, auth)
-//     })
-// })
+        console.log(data[0])
+        res.redirect(`/`)
+    })
+})
 
 app.get('/cadastro', function(req, res){
     res.render('cadastro')
